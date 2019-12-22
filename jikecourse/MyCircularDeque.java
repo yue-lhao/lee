4d1ed6450756920aa559de34ba0014321bf53095
koshiro <759302952@qq.com>
2019-12-22 15:39:42 +0800

    双端队列

---
+++ jikecourse/MyCircularDeque.java
@@ -0,0 +1,113 @@
+package com.xiaohao.jike;
+
+/**
+ * 设计实现双端队列。
+ * 你的实现需要支持以下操作：
+ *
+ * MyCircularDeque(k)：构造函数,双端队列的大小为k。
+ * insertFront()：将一个元素添加到双端队列头部。 如果操作成功返回 true。
+ * insertLast()：将一个元素添加到双端队列尾部。如果操作成功返回 true。
+ * deleteFront()：从双端队列头部删除一个元素。 如果操作成功返回 true。
+ * deleteLast()：从双端队列尾部删除一个元素。如果操作成功返回 true。
+ * getFront()：从双端队列头部获得一个元素。如果双端队列为空，返回 -1。
+ * getRear()：获得双端队列的最后一个元素。 如果双端队列为空，返回 -1。
+ * isEmpty()：检查双端队列是否为空。
+ * isFull()：检查双端队列是否满了。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-circular-deque
+ */
+public class MyCircularDeque {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int[] data;
+    private int capacity;
+    private int head;
+    private int tail;
+
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    public MyCircularDeque() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public MyCircularDeque(int k) {
+        data = new int[k+1];
+        capacity = k+1;
+    }
+
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    public boolean insertFront(int value) {
+        if (isFull()) return false;
+        head = (head - 1 + capacity) % capacity;
+        data[head] = value;
+        return true;
+    }
+
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        data[tail] = value;
+        tail = (tail + 1) % capacity;
+        return true;
+    }
+
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        head = (head + 1) % capacity;
+        return true;
+    }
+
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        tail = (tail - 1 + capacity) % capacity;
+        return true;
+    }
+
+    /** Get the front item from the deque. */
+    public int getFront() {
+        if (isEmpty()) return -1;
+        return data[head];
+    }
+
+    /** Get the last item from the deque. */
+    public int getRear() {
+        if (isEmpty()) return -1;
+        return data[(tail - 1 + capacity) % capacity];
+    }
+
+    /** Checks whether the circular deque is empty or not. */
+    public boolean isEmpty() {
+        return head == tail;
+    }
+
+    /** Checks whether the circular deque is full or not. */
+    public boolean isFull() {
+        return (tail + 1) % capacity == head;
+    }
+
+    public static void main(String[] args) {
+        MyCircularDeque circularDeque = new MyCircularDeque(3); // 设置容量大小为3
+        /*circularDeque.insertLast(1);			        // 返回 true
+        circularDeque.insertLast(2);			        // 返回 true
+        circularDeque.insertFront(3);			        // 返回 true
+        circularDeque.insertFront(4);			        // 已经满了，返回 false
+        circularDeque.getRear();  				// 返回 2
+        circularDeque.isFull();				        // 返回 true
+        circularDeque.deleteLast();			        // 返回 true
+        circularDeque.insertFront(4);			        // 返回 true
+        circularDeque.getFront();*/				// 返回 4
+
+        System.out.println(circularDeque.insertLast(1));
+        System.out.println(circularDeque.insertLast(2));
+        System.out.println(circularDeque.insertFront(3));
+        System.out.println(circularDeque.insertFront(4));
+        System.out.println(circularDeque.getRear());
+        System.out.println(circularDeque.isFull());
+        System.out.println(circularDeque.deleteLast());
+        System.out.println(circularDeque.insertFront(4));
+        System.out.println(circularDeque.getFront());
+    }
+
+}
